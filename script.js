bc051ad3057069e6af2d28be0ff87b9e3f86cde4
Oforduhh <oforduhh@gmail.com>
2022-08-25 22:18:11 +0100

    finished the js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const light_mode = document.querySelector("#toggle-on");
 const dark_mode = document.querySelector("#toggle-off");
 const error_message = document.querySelector(".error_message");
+const tip_amount_container = document.querySelector(".tip_amount span");
 
 light_mode.addEventListener("click", function () {
   document.body.classList.toggle("dark-mode");
@@ -23,8 +24,8 @@ const qualitySelector = document.querySelector(".quality");
 const calculateButton = document.querySelector(".calc");
 const Tip_amount = document.querySelector(".tip_amount");
 
-// this function handles the tip amount
-calculateButton.addEventListener("click", function () {
+// this function handles the tip amount calculations
+calculateButton.addEventListener("click", async () => {
   // fetch all values of each form input
   let amountValue = amountSelector.value;
   let guestValue = guestSelector.value;
@@ -35,48 +36,62 @@ calculateButton.addEventListener("click", function () {
   guestValue = parseInt(guestValue);
   qualityValue = parseFloat(qualityValue);
 
-  console.log(typeof amountValue);
-  console.log(typeof guestValue);
-  handleValidation({ amountValue, guestValue, qualityValue });
-  // checks if it is not a number or if the input field is empty
-  //typeof checks the data type of a value
-  return;
-  if (typeof amountValue !== "number" || !amountValue) {
-    // if the above statement is true, throw this error message and stop the code
-    // return statement is use to stop or break out of a code
-    HandleErrorMessage(amountSelector, "is required and must be a number");
-    return;
-  }
+  // validate the value of each input field and show the error message
+  error_message.style.display = "block";
+  const validators = [
+    handleValidation(amountValue, amountSelector),
+    handleValidation(guestValue, guestSelector),
+    handleValidation(qualityValue, qualitySelector),
+  ];
 
-  //   const calc = ((Amount.value * Quality.value) / Guest.value).toFixed(2);
-  //   console.log(calc);
-  //   console.log(Tip_amount);
-  //   Amount.value = "";
-  //   Quality.value = "";
-  //   Guest.value = "";
+  // loop through the validators and ensure they all pass the validation test
+  const validationComplete = validators.every((validator) => validator);
 
-  //   if (calc === "NaN") {
-  //     Tip_amount.style.display = "none";
-  //     alert("This is working");
-  //     return;
-  //   } else {
-  //     Tip_amount.style.display = "block";
-  //     Tip_amount.innerHTML = calc;
-  //     Tip_amount.style.padding = "15px";
-  //     Tip_amount.style.color = "black";
-  //   }
+  //breaks the code if validation fails, this ensure the calc function does not run
+  if (!validationComplete) return;
+
+  // this block of code calls calculate function
+  const calc = handleTipCalculation(amountValue, qualityValue, guestValue);
+
+  // set the return value after calculation to the tip_amount container
+  tip_amount_container.innerText = calc;
+
+  //clear the value in the input field
+  amountSelector.value = "";
+  guestSelector.value = "";
+  qualitySelector.value = "";
 });
 
-const handleValidation = ({ amountValue, guestValue, qualityValue }) => {
-  console.log(amountValue, guestValue, qualityValue);
+// This functionality validate the value of each input field
+const handleValidation = (inputValue, inputSelector) => {
+  // checks if it's NAN or the field is an empty
+  if (isNaN(inputValue)) {
+    // if the above statement is true, throw this error message and stop the code
+    // return statement is use to stop or break out of a code
+    handleErrorMessage(inputSelector, "cannot not be an empty field");
+    return;
+  }
+  // checks if the value is zero
+  if (inputValue === 0) {
+    handleErrorMessage(inputSelector, "must be greater than zero (0)");
+    return;
+  }
+  return true;
 };
 
 // this functionality takes in the input field and the custom error message
-const HandleErrorMessage = (selector, errorMessage) => {
+const handleErrorMessage = (selector, errorMessage) => {
   const inputContainer = selector.parentElement;
   const formGroup = inputContainer.parentElement;
   const label = formGroup.querySelector("label");
   error_message.style.display = "block";
   error_message.innerText = `${label.innerText} ${errorMessage}`;
-  //
+};
+
+// a function that calculate the tip amount and returns the calculated value
+const handleTipCalculation = (a, b, c) => {
+  // hide the display message
+  error_message.style.display = "none";
+  const calculate = (a * b) / c;
+  return calculate.toFixed(2);
 };
